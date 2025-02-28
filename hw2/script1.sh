e2267ec9ac3c555e75738bfc380cf273b2fa064c
Anna Fadeeva <151925079+AnnaFad@users.noreply.github.com>
2025-02-28 23:14:21 +0300

    Update script1.sh

--- hw2/script1.sh
+++ hw2/script1.sh
@@ -1,7 +1,7 @@
-scp mapred-site.xml tmpl-dn-00:/home/hadoop/hadoop-3.4.0/etc/hadoop
-scp mapred-site.xml tmpl-dn-01:/home/hadoop/hadoop-3.4.0/etc/hadoop
-scp mapred-site.xml tmpl-nn:/home/hadoop/hadoop-3.4.0/etc/hadoop
+scp hadoop-3.4.0/etc/hadoop/mapred-site.xml tmpl-dn-00:/home/hadoop/hadoop-3.4.0/etc/hadoop
+scp hadoop-3.4.0/etc/hadoop/mapred-site.xml tmpl-dn-01:/home/hadoop/hadoop-3.4.0/etc/hadoop
+scp hadoop-3.4.0/etc/hadoop/mapred-site.xml tmpl-nn:/home/hadoop/hadoop-3.4.0/etc/hadoop
 
-scp yarn-site.xml tmpl-dn-00:/home/hadoop/hadoop-3.4.0/etc/hadoop
-scp yarn-site.xml tmpl-dn-01:/home/hadoop/hadoop-3.4.0/etc/hadoop
-scp yarn-site.xml tmpl-nn:/home/hadoop/hadoop-3.4.0/etc/hadoop
+scp hadoop-3.4.0/etc/hadoop/yarn-site.xml tmpl-dn-00:/home/hadoop/hadoop-3.4.0/etc/hadoop
+scp hadoop-3.4.0/etc/hadoop/yarn-site.xml tmpl-dn-01:/home/hadoop/hadoop-3.4.0/etc/hadoop
+scp hadoop-3.4.0/etc/hadoop/yarn-site.xml tmpl-nn:/home/hadoop/hadoop-3.4.0/etc/hadoop
