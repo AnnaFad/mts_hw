4d3e9b71668d2fb4d727caeec0e7aab1e464afb5
Anna Fadeeva <151925079+AnnaFad@users.noreply.github.com>
2025-02-28 23:04:38 +0300

    Update script1.sh

--- hw2/script1.sh
+++ hw2/script1.sh
@@ -5,6 +5,3 @@ scp mapred-site.xml tmpl-nn:/home/hadoop/hadoop-3.4.0/etc/hadoop
 scp yarn-site.xml tmpl-dn-00:/home/hadoop/hadoop-3.4.0/etc/hadoop
 scp yarn-site.xml tmpl-dn-01:/home/hadoop/hadoop-3.4.0/etc/hadoop
 scp yarn-site.xml tmpl-nn:/home/hadoop/hadoop-3.4.0/etc/hadoop
-
-hadoop-3.4.0/sbin/start-yarn.sh
-mapred --daemon start historyserver
